b6bc07370392fee5c34cde594e52ca0f3664df27
Greta Wang <gwang@smith.edu>
2025-09-25 16:44:23 -0400

    final draft

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,59 @@
 
+import java.util.Scanner;
+import java.util.Random;
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  String[] transcript;
 
   /**
    * Constructor 
    */
   Conversation() {
-    
+    this.transcript = new String[0];
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner input = new Scanner(System.in);
 
+    // Ask how many rounds and set appropriate transcript length
+    System.out.print("How many rounds? ");
+    int rounds = input.nextInt();
+    input.nextLine();
+    this.transcript = new String[rounds*2 + 2];
+
+    // Print greeting
+    System.out.println("Hi there!  What's on your mind?");
+    this.transcript[0] = "Hi there!  What's on your mind?";
+    
+    // Carry out conversation
+    String userStatement = "";
+    for (int i = 1; i < rounds*2; i += 2){
+      userStatement = input.nextLine();
+      this.transcript[i] = userStatement;
+      String response = respond(userStatement);
+      System.out.println(response);
+      this.transcript[i+1] = response;
+    }
+    input.close();
+
+    // Print goodbye
+    System.out.println("See ya!");
+    this.transcript[rounds*2 + 1] = "See ya!";
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println("\nTRANSCRIPT:");
+    for (int i = 0; i < this.transcript.length; i++){
+      System.out.println(this.transcript[i]);
+    }
   }
 
   /**
@@ -30,7 +62,89 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    // Remove punctuation from end of input, if present
+    String lastChar = inputString.substring(inputString.length() - 1, inputString.length());
+    if (lastChar.equals(".") || lastChar.equals("!") || lastChar.equals("?")){
+      inputString = inputString.substring(0, inputString.length() - 1);
+    }
+
+    // Split input into Array of words, mirror each word if needed
+    String[] words = inputString.split(" ");
+    int nonMirrorWords = 0;
+    for (int i = 0; i < words.length; i++){
+      if (words[i].equals("I")){
+        if (i == 0){
+          words[i] = "You";
+        }
+        else {
+          words[i] = "you";
+        }
+      }
+      else if (words[i].equals("me")){
+        words[i] = "you";
+      }
+      else if (words[i].equals("Me")){
+        words[i] = "You";
+      }
+      else if (words[i].equals("am")){
+        words[i] = "are";
+      }
+      else if (words[i].equals("Am")){
+        words[i] = "Are";
+      }
+      else if (words[i].equals("are")){
+        words[i] = "am";
+      }
+      else if (words[i].equals("Are")){
+        words[i] = "Am";
+      }
+      else if (words[i].equals("you") || words[i].equals("You")){
+        words[i] = "I";
+      }
+      else if (words[i].equals("my")){
+        words[i] = "your";
+      }
+      else if (words[i].equals("My")){
+        words[i] = "Your";
+      }
+      else if (words[i].equals("your")){
+        words[i] = "my";
+      }
+      else if (words[i].equals("Your")){
+        words[i] = "My";
+      }
+      else if (words[i].equals("you're") || words[i].equals("You're")){
+        words[i] = "I'm";
+      }
+      else if (words[i].equals("I'm")){
+        if (i == 0){
+          words[i] = "You're";
+        }
+        else {
+          words[i] = "you're";
+        }
+      }
+      else {
+        // Count number of words that are not mirrored
+        nonMirrorWords += 1;
+      }
+    }
+
+    String returnString = "";
+    // If no words were mirrored, choose canned response
+    if (nonMirrorWords == words.length){
+      String[] cannedResponses = {"Mmm-hm.", "Cool!", "Uh huh.", "I see.", "Gotcha."};
+      Random random = new Random();
+      returnString = cannedResponses[random.nextInt(cannedResponses.length)];
+    }
+    // If words were mirrored, concatenate Array elements back into a String and add question mark
+    else {
+      for (int i = 0; i < words.length; i++){
+        returnString = returnString.concat(words[i] + " ");
+      }
+      returnString = returnString.substring(0, returnString.length() - 1); // Remove extra space from end
+      returnString = returnString.concat("?");
+    }
     return returnString; 
   }
 
--- reflection.md
+++ reflection.md
@@ -1,7 +1,25 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
+I consulted with Diane Tuyizere and Grace Pepperman. They showed me other ways of thinking about the assignment; both of them used contain and replace to mirror words, which I hadn't tried. 
+
+
 - Which session(s) of TA / office hours did you attend?
+
+I attended TA hours in the evening on Sunday 9/21. The TA helped me with for loops in Java. Specifically, I learned that the only way to iterate through an array is by using the indexes, rather than going through the items directly like you can in Python.
+
+
 - What are your initial impressions of Java? 
+
+It feels less intuitive to me than Python, but I'm figuring it out. It seems like some features, such as printing and getting input, have longer and more complicated processes than the Python equivalent. I don't understand yet what all the syntax means, which can make it confusing.
+
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
+Python and Java have very different syntax, but it seems like the basic tasks each can accomplish are similar. For this assignment, I was able to reason through solving it based on my knowledge of Python, and then figure out the equivalent Java syntax. Thus, I would conclude that programming in general is based on certain basic principles, which may look different depending on the program.
+
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+This assignment went pretty smoothly. At first, I had trouble using Scanner objects, but the textbook helped with this. For mirroring words, I used the first approach that occurred to me, which was to split the message into a list of words and mirror each one before concatenating them back together. This worked well but took many lines of code and may not have been the most efficient; if I were to do it again, I would consider other options before choosing. I looked at the official Java documentation while doing this assignment, and I'd recommend it to future students as a resource for String methods, etc., since some are different from Python.
